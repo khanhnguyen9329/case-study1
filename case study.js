c96d15cee1bbb2ee5ab2b7ef2f35312229df7f70
khanhnguyen <khanhnguyen9329@gmail.com>
2022-08-01 10:11:19 +0700

    casestudy

---
+++ case study.js
@@ -0,0 +1,13 @@
+function numberone() {
+    let username  = document.getElementById("paper").value;
+    let password = document.getElementById("bomber").value;
+    if(username=="khanh9999" && password==123456) {
+        alert("đăng nhập thành công")
+    window.open("https://www.honda.com.vn/tin-tuc/xe-may")
+
+    }else {
+        alert("nhập username vs password")
+    }
+
+
+}
